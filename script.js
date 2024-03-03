3172f4d8d6e8c3d064d0a506892355d1d760d655
Gug4ZL <gustavo2004ct@gmail.com>
2024-03-03 13:28:55 -0300

    adicinonado java script

---
+++ script.js
@@ -0,0 +1,13 @@
+const form = document.querySelector(".formulario")
+
+
+
+const abrir = () =>{
+    form.style.opacity = 1
+    form.style.left = "50%"
+}
+
+const fechar = () =>{
+    form.style.opacity = 0
+    form.style.left = "-21%"
+}
